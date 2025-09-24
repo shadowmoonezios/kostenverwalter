23960448c4a4177162f20f59e688b4c45258e67f
shadowmoonezios <agushifelde@hotmail.com>
2025-09-24 17:10:20 +0300

    feat: initial commit for frontend/src/App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Kostenverwalter</h1>
+      <p>Willkommen zur Kostenverwalter-App!</p>
+    </div>
+  );
+}
+
+export default App;
